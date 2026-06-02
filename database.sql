61b01c197ff44ba742a13be8295cf5639312d699
kndaruma <kn.daruma@gmail.com>
2026-06-02 20:37:38 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -10,6 +10,10 @@ CREATE TABLE IF NOT EXISTS `my_properties` (
     `owner` VARCHAR(50) DEFAULT NULL,
     `price` INT(11) DEFAULT -1,
     `donate_expire` BIGINT DEFAULT 0,
+    `is_rentable` TINYINT(1) DEFAULT 0,
+    `rent_price_per_day` INT(11) DEFAULT 0,
+    `renter` VARCHAR(50) DEFAULT NULL,
+    `rent_expire` BIGINT DEFAULT 0,
     PRIMARY KEY (`id`),
     KEY `idx_dimension` (`dimension`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -23,6 +27,7 @@ CREATE TABLE IF NOT EXISTS `my_property_furniture` (
     `coords` LONGTEXT NOT NULL,
     `rot` LONGTEXT NOT NULL,
     `no_collision` TINYINT(1) DEFAULT 0,
+    `is_frozen` TINYINT(1) DEFAULT 1,
     `dimension` INT(11) NOT NULL,
     PRIMARY KEY (`id`, `house_id`),
     KEY `idx_house_id` (`house_id`)
